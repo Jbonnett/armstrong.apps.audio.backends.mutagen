55c644d520ed80088bdecafcc0640c1185374a21
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-18 15:52:39 -0700

    made it use the new test_suite option

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,5 @@
 import unittest
-from fabric.decorators import *
+from armstrong.dev.tasks import *
 from armstrong.apps.audio.backends.mutagen.tests import MutagenTestCase
 
-@task 
-def test():
-    suite = unittest.TestLoader().loadTestsFromTestCase(MutagenTestCase)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+test_suite = unittest.TestLoader().loadTestsFromTestCase(MutagenTestCase)
